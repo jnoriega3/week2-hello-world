0ae27266f69b573e6f9d46a33728a7286017307c
Jessica Noriega <jnoriega3@mail.valenciacollege.edu>
2016-01-20 23:05:04 -0500

    Week 2
    Week 2

--- helloworld.py
+++ helloworld.py
@@ -2,9 +2,18 @@
 
 # write a program that:
 # 1. greets the user in English
-# 2. asks the user to choose from 1 of 3 spoken languages (pick your favorite languages!) 
+print('Hello')
+
+# 2. asks the user to choose from 1 of 3 spoken languages (pick your favorite languages!)
+print('Please select a language to be greeted in that language.')
+
 # 3. displays the greeting in the chosen language
+print('1. English')
+print('2. Spanish')
+print('3. Portuguese')
+
 # 4. exits
+print('Thank you, Goodbye!')
 
 # make sure that your code contains comments explaining your logic!
 
