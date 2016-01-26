b70e81e4bc32b3cc7c2a6d16c4a16ff55bc8aec2
Jessica Noriega <jnoriega3@mail.valenciacollege.edu>
2016-01-26 14:34:02 -0500

    Updated corrections Week 2 Hello World
    I forgot about the input() call in my original submission for user input. I believe the program should work fine now.

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,22 @@
-# replace the contents of this comment with your full name
+# Jessica Noriega
 
 # write a program that:
 # 1. greets the user in English
+print('Hello!')
 # 2. asks the user to choose from 1 of 3 spoken languages (pick your favorite languages!) 
+print('Please Enter 1 to be greeted in English.')
+print('Please Enter 2 to be greeted in Spanish.')
+print('Please Enter 3 to be greeted in Portuguese.')
 # 3. displays the greeting in the chosen language
+lan = input()
+if lan == '1':
+    print('Hello!')
+elif lan == '2':
+    print('Hola!')
+elif lan == '3':
+    print('Oi!')
+
 # 4. exits
-
+print('Thank you, Goodbye!')
+      
 # make sure that your code contains comments explaining your logic!
-
